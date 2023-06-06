df2e99bb11839817e1580612a8bcd3ae7a545cf4
lisa_ywl <yourworthlesslife@icloud.com>
2023-06-07 02:10:23 +0300

    first attemt to make pretty hamburger menu

--- index.js
+++ index.js
@@ -16,3 +16,9 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
